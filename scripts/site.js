7a7f1c19d0166ff3cc53806d449b0001ab3397e0
Addie <azm16@macbookpro-73f6.up.corp.upc>
2016-11-08 10:19:44 -0500

    initial commit

---
+++ scripts/site.js
@@ -0,0 +1,37 @@
+// Main JS should go here!
+// Include scripts using Browserify by doing:
+// var $ = require("jquery");
+
+
+// var el = document.querySelector('#test');
+// var text = document.querySelector('.banner-article__header');
+// var header = document.querySelector('.article-list__header');
+
+// text.addEventListener('click', function () {
+//     console.log('click!');
+
+//     if (el.classList.contains('banner-article')) {
+//         el.classList.remove('banner-article');
+//         el.classList.add('hidden');
+//     }
+// });
+
+// header.addEventListener('click', function () {
+//     console.log('Hey');
+
+//     if (el.classList.contains('hidden')) {
+//         el.classList.remove('hidden');
+//         el.classList.add('banner-article');
+//     }
+// });
+
+// var filter = document.querySelector('#filter-design');
+// var designArticle = document.querySelector('#design-article');
+// var elements = document.querySelector('*');
+
+// filter.addEventListener('click', function () {
+//     if (!elements.classList.contains('design-article')) {
+//         elements.classList.add('hide');
+//     }
+// });
+
