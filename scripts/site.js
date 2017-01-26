aed359ea8410c08f036ff7ee902d6a9e113b2382
Addie <azm16@macbookpro-73f6.up.corp.upc>
2017-01-26 11:02:53 -0500

    adds jquery slick

--- scripts/site.js
+++ scripts/site.js
@@ -3,35 +3,11 @@
 // var $ = require("jquery");
 
 
-// var el = document.querySelector('#test');
-// var text = document.querySelector('.banner-article__header');
-// var header = document.querySelector('.article-list__header');
-
-// text.addEventListener('click', function () {
-//     console.log('click!');
-
-//     if (el.classList.contains('banner-article')) {
-//         el.classList.remove('banner-article');
-//         el.classList.add('hidden');
-//     }
-// });
-
-// header.addEventListener('click', function () {
-//     console.log('Hey');
-
-//     if (el.classList.contains('hidden')) {
-//         el.classList.remove('hidden');
-//         el.classList.add('banner-article');
-//     }
-// });
-
-// var filter = document.querySelector('#filter-design');
-// var designArticle = document.querySelector('#design-article');
-// var elements = document.querySelector('*');
-
-// filter.addEventListener('click', function () {
-//     if (!elements.classList.contains('design-article')) {
-//         elements.classList.add('hide');
-//     }
-// });
-
+$('.carousel').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    dots: true,
+    mobileFirst: true,
+    swipeToSlide: true
+});
