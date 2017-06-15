ace7277a8b792ad9983a1c9c4cded546bc774ebb
Addie Padula <rebekah.adair@yahoo.com>
2017-06-15 08:13:47 -0400

    adds overlay code

--- scripts/site.js
+++ scripts/site.js
@@ -12,3 +12,30 @@ $('.carousel-container').slick({
     mobileFirst: true,
     swipeToSlide: true
 });
+
+
+const teamMembers = document.querySelectorAll('.team__member');
+const images = document.querySelectorAll('.team__image');
+const content = document.querySelectorAll('.team__content');
+
+const teamArray = Array.from(teamMembers);
+const imageArray = Array.from(images);
+
+
+images.forEach(function(imageEl) {
+  imageEl.addEventListener('mouseenter', applyOverlay);
+  imageEl.addEventListener('mouseleave', removeOverlay);
+});
+
+function applyOverlay(e) {
+  teamArray.forEach(function(teamEl) {
+    teamEl.setAttribute('data-active', 'true');
+  })
+  e.target.parentNode.setAttribute('data-active', 'false');
+}
+
+function removeOverlay(e) {
+  teamArray.forEach(function(teamEl) {
+    teamEl.setAttribute('data-active', 'false');    
+  })
+}
