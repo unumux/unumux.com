5158c14c5cd62e8e0a139b27c289ba7385ebdfb1
Addie Padula <rebekah.adair@yahoo.com>
2017-06-30 14:31:46 -0400

    adds mobile animation, refactors for mobile to desktop styles

--- scripts/site.js
+++ scripts/site.js
@@ -39,3 +39,33 @@ images.forEach(function(imageEl) {
 imageEl.addEventListener('mouseenter', applyOverlay);
 imageEl.addEventListener('mouseleave', removeOverlay);
 });
+
+
+// TEAM PAGE CODE
+
+const contents = document.querySelectorAll('.team__content');
+const images2 = document.querySelectorAll('.team__image');
+const body = document.querySelector('body');
+
+function controlModal(e) {
+    console.log(e.target);
+    // hide all other modals
+    if(window.innerWidth < 768) { // ask carl about this
+        contents.forEach((content) => {
+            content.setAttribute('data-active', 'false');
+            // body.classList.remove('overlay');
+        });
+
+        // need a way to hide all modals when any other element is clicked 
+
+        // get content for clicked team member
+        const currentContent = e.target.nextElementSibling;
+        // if not currently active, make it active
+        if(currentContent.dataset.active === 'false') {
+        currentContent.setAttribute('data-active', 'true');
+        //   body.classList.add('overlay');
+        }
+    }
+}
+
+images2.forEach(image => image.addEventListener('click', controlModal));
