ecf4e742322cf2f69b18f484ff8338903a2aee99
Addie Padula <rebekah.adair@yahoo.com>
2017-07-11 16:19:20 -0400

    adds close modal functionality

--- scripts/site.js
+++ scripts/site.js
@@ -45,27 +45,42 @@ imageEl.addEventListener('mouseleave', removeOverlay);
 
 const contents = document.querySelectorAll('.team__content');
 const images2 = document.querySelectorAll('.team__image');
-const body = document.querySelector('body');
+const modals = document.querySelectorAll('.team__modal');
 
 function controlModal(e) {
-    console.log(e.target);
+    // console.log(e.target);
     // hide all other modals
-    if(window.innerWidth < 768) { // ask carl about this
-        contents.forEach((content) => {
-            content.setAttribute('data-active', 'false');
-            // body.classList.remove('overlay');
-        });
+    modals.forEach((modal) => {
+        modal.setAttribute('data-active', 'false');
+    });
 
-        // need a way to hide all modals when any other element is clicked 
+    if(window.innerWidth < 768) { // account for when user activates js then makes screen bigger ?? check carls' slack for js
+
+        // need a way to hide all modals and remove overlay when any other element is clicked
+        
+
 
         // get content for clicked team member
         const currentContent = e.target.nextElementSibling;
         // if not currently active, make it active
         if(currentContent.dataset.active === 'false') {
-        currentContent.setAttribute('data-active', 'true');
-        //   body.classList.add('overlay');
+            currentContent.setAttribute('data-active', 'true');
         }
     }
 }
 
+function killEverything(e) {
+    modals.forEach(modal => {
+        if(modal.dataset.active === 'true') {
+            console.log("kill everything!!");
+            modal.setAttribute('data-active', 'false');
+        }
+    });
+}
+
+
+modals.forEach(modal => modal.addEventListener('click', killEverything));
 images2.forEach(image => image.addEventListener('click', controlModal));
+const buttons = document.querySelectorAll('.button');
+buttons.forEach(button => button.addEventListener('click', killEverything), false);
+// document.body.addEventListener('click', killEverything);
